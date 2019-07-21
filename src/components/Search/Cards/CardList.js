f8ac0086bbbf251f4681dc057b70a9e81ca9cac2
Dayne <daynewr@gmail.com>
2019-07-21 01:10:04 -0500

    remove unused deps

--- src/components/Search/Cards/CardList.js
+++ src/components/Search/Cards/CardList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Card, Message } from 'semantic-ui-react';
 import SingleCard from './SingleCard';
